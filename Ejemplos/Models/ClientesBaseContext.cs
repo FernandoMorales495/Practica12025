77d8a249f9eca3356ae5b9ce0b3d237bf5071bbb
LUIS FERNANDO MORALES PEREZ <Fernandomoe09@outlook.com>
2025-03-21 18:54:31 -0600

    cambio1

---
+++ Ejemplos/Models/ClientesBaseContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ejemplos.Models;
+
+public partial class ClientesBaseContext : DbContext
+{
+    public ClientesBaseContext()
+    {
+    }
+
+    public ClientesBaseContext(DbContextOptions<ClientesBaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Clientes__3214EC079A31A61C");
+
+            entity.Property(e => e.Direccion).HasMaxLength(255);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
