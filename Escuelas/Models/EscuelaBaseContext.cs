217c3cf576b443e949952bac84f854d80d498efb
LUIS FERNANDO MORALES PEREZ <Fernandomoe09@outlook.com>
2025-03-21 20:43:19 -0600

    Cambios

---
+++ Escuelas/Models/EscuelaBaseContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Escuelas.Models;
+
+public partial class EscuelaBaseContext : DbContext
+{
+    public EscuelaBaseContext()
+    {
+    }
+
+    public EscuelaBaseContext(DbContextOptions<EscuelaBaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Estudiante> Estudiantes { get; set; }
+
+   
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Estudiante>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Estudian__3214EC27E407F5CF");
+
+            entity.ToTable("Estudiante");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.CorreoElectronico)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaInscripcion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Promedio).HasColumnType("decimal(4, 2)");
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Escuelas/Models/Estudiante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Escuelas.Models;
+
+public partial class Estudiante
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public DateOnly FechaNacimiento { get; set; }
+
+    public string? CorreoElectronico { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public int Grado { get; set; }
+
+    public decimal? Promedio { get; set; }
+
+    public DateTime? FechaInscripcion { get; set; }
+}
