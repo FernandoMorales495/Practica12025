636411066516c8e68088ae6ebdd129b1a5b4a7a6
LUIS FERNANDO MORALES PEREZ <Fernandomoe09@outlook.com>
2025-03-21 21:27:48 -0600

    cambios

---
+++ Categorias/Models/CategoriasBaseContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Categorias.Models;
+
+public partial class CategoriasBaseContext : DbContext
+{
+    public CategoriasBaseContext()
+    {
+    }
+
+    public CategoriasBaseContext(DbContextOptions<CategoriasBaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categorias { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Categori__3214EC0731006ABC");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
